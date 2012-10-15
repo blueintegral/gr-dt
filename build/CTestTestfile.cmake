965a058952c7012cd733741c54aec2c7ab30b45c
Live session user <ubuntu@ubuntu.(none)>
2012-10-15 07:38:03 -0700

    removed build from tracking

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/src/gr-dt
-# Build directory: /home/ubuntu/src/gr-dt/build
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(python)
-SUBDIRS(grc)
